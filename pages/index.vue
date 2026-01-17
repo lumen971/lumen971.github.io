b1e610215d66e335af4211eb8248e970ca4893a9
Mido <midorin6497@gmail.com>
2026-01-17 14:43:17 +0800

    git init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.index
+  p test
+</template>
+
+<style lang="scss">
+.index{
+  p{
+    color:aquamarine;
+  }
+}
+</style>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
